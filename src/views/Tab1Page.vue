10219d15bea4e379dea1b07dc3e8f8a039bd6bd3
rene <0228505@up.edu.mx>
2022-05-19 08:30:46 -0500

    parte 2

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -1,3 +1,51 @@
+
+
+<script>
+import { defineComponent } from 'vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCol, IonGrid, IonRow } from '@ionic/vue';
+import { getDatabase, ref, onValue } from "firebase/database";
+
+export default defineComponent({
+  name: 'Tab1Page',
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonCol, IonGrid, IonRow },
+  mounted() {
+    const db = getDatabase();
+    const starCountRef = ref(db, "claves/");
+    onValue(starCountRef, (snapshot) => {
+      const data = snapshot.val();
+      console.log(data);
+      var cont = 0
+      snapshot.forEach(element => {
+        this.listaKeys[cont] = element.key
+        this.listaClaves[cont] = element.toJSON()
+        cont++
+      });
+    });
+    console.log("lista de claves", this.listaCaves)
+  },
+  data() {
+    return {
+      listaClaves: [{ status: "", usuario: "" }],
+      listaKeys: []
+    }
+  }
+
+});
+</script>
+<!--  -->
+<style>
+:root {
+  --ion-safe-area-top: 20px;
+  --ion-safe-area-bottom: 22px;
+}
+
+ion-col>div {
+  background-color: #f7f7f7;
+  border: solid 1px #ddd;
+  padding: 10px;
+}
+
+</style>
 <template>
   <ion-page>
     <ion-header>
@@ -6,24 +54,32 @@
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
+      <ion-grid>
+        <ion-row>
+          <ion-col>
+            <div>
+              Clave
+            </div>
+          </ion-col>
+          <ion-col>
+            <div>
+              Status
+            </div>
+          </ion-col>
+        </ion-row>
+        <ion-row v-for="(item,index) in listaClaves" :key="index"> 
+          <ion-col >
+            <div>
+              {{listaKeys[index]}}
+            </div>
+          </ion-col>
+          <ion-col>
+            <div>
+              {{item.status}}
+            </div>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
     </ion-content>
   </ion-page>
 </template>
-
-<script>
-import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
-
-export default  defineComponent({
-  name: 'Tab1Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
-});
-</script>
